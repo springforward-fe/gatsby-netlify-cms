3c3cd1cc4a1c59fe5d3f7e44d8a8732676c2cb0f
springforward-fe <springforward.fe@gmail.com>
2018-10-21 16:24:00 -0700

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.png'
 
 const Navbar = () => (
@@ -19,20 +18,8 @@ const Navbar = () => (
           {/*Products*/}
         {/*</Link>*/}
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
     </div>
   </nav>
-)
+);
 
 export default Navbar
